b1e0d0b08b53bd132fe08d3e1462f8f4b654111b
SHUBHAM JANGAM <jangamshubham273@gmail.com>
2020-03-07 20:08:16 +0530

    Changed Const str to str

--- 09 Texture Kundali and Pyramid/window.mm
+++ 09 Texture Kundali and Pyramid/window.mm
@@ -513,7 +513,7 @@ main(int argc , const char *argv[])
             0.0f, 0.0f
     };
 
-    const GLfloat fcubeVertices[] = {
+    GLfloat fcubeVertices[] = {
             1.0f, 1.0f, -1.0f,
             -1.0f, 1.0f, -1.0f,
             -1.0f, 1.0f, 1.0f,
